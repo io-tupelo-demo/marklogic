805a44c0eca16875c6f0a85989e4e1ef1b2d481c
Alan Thompson <athom555@mcna.net>
2025-07-18 06:26:18 -0700

    misc

--- go-init.bash
+++ go-init.bash
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 set -v
-# podman \
-#   progressofficial/marklogic-db:11.3.1-ubi9-rootless-2.1.3
 
 docker \
      run -d -it -p 8000:8000 -p 8001:8001 -p 8002:8002 \
      -e MARKLOGIC_INIT=true \
      -e MARKLOGIC_ADMIN_USERNAME='admin' \
      -e MARKLOGIC_ADMIN_PASSWORD='admin' \
-     progressofficial/marklogic-db:latest
+     docker.repo1.uhc.com/progressofficial/marklogic-db
 
