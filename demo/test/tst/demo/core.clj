16bc20d08edf43b39e5d0d1f8a28d3f15e1d7925
Alan Thompson <cloojure@gmail.com>
2025-07-21 20:28:57 -0700

    misc

--- demo/test/tst/demo/core.clj
+++ demo/test/tst/demo/core.clj
@@ -10,21 +10,7 @@
     [tupelo.string :as str]
     ))
 
-(dotest
+(verify
   (is= 5 (+ 3 2))
-
-  (is= 7 (add2-public 4 3)) ; normal public access
-  (is= 7 (#'demo.core/add2-private 4 3)) ; use fully-qualified Var for private access
-
-  (is= [:a 1 :b 2] (mapv edn/read-string [":a" "1" ":b" "2"]))
-  (is= {:a 1 :b 2}
-    (hash-map :a 1 :b 2) ; 4 scalar args
-    (apply hash-map [:a 1 :b 2])) ; a single vector arg
-
-  ; cannot coerce data into a map via these expressions
-  (throws? (into {} :a 1 :b 2))
-  (throws? (into {} [:a 1 :b 2]))
-  (throws? (hash-map [:a 1 :b 2]))
-  (throws? (edn/read-string [":a" "1" ":b" "2"]))
 
   )
