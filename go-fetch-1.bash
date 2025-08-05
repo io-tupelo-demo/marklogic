0f7372d624a0be9d645a28f274eb94b78be2039f
Alan Thompson <athom555@mcna.net>
2025-08-05 05:48:04 -0700

    misc

--- go-fetch-1.bash
+++ go-fetch-1.bash
@@ -3,5 +3,5 @@ set -v
 
 curl -v --anyauth  --user admin:admin   \
     -H "Accept: application/json"       \
-    'http://localhost:8000/LATEST/documents?uri=/Users/athom555/expr/marklogic/data/cust3.json'
+    'http://localhost:8000/LATEST/documents?uri=/Users/athom555/expr/marklogic/data/cust1.json'
 
